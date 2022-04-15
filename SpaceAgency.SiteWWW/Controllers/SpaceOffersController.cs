eff04fd582b5666d490aa34ac67a0dacb0c2f746
rafmoc <olejniczak00@gmail.com>
2022-04-15 17:51:16 +0200

    Adding shop part

--- SpaceAgency.SiteWWW/Controllers/SpaceOffersController.cs
+++ SpaceAgency.SiteWWW/Controllers/SpaceOffersController.cs
@@ -1,5 +1,6 @@
 ﻿using SpaceAgency.Data.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace SpaceAgency.SiteWWW.Controllers
 {
@@ -14,7 +15,21 @@ namespace SpaceAgency.SiteWWW.Controllers
 
         public async Task<IActionResult> Index(int? id)
         {
-            return View();
+            ViewBag.ModelRockets = await _context.Rocket.ToListAsync();
+
+            if (id == null)
+            {
+                var first = await _context.Rocket.FirstAsync();
+                id = first.IdRocket;
+            }
+
+            return View(await _context.Engine.Where(t => t.IdRocket == id).ToListAsync());
+        }
+
+        public async Task<IActionResult> MoreInfo(int? id)
+        {
+            ViewBag.ModelRockets = await _context.Rocket.ToListAsync();
+            return View(await _context.Engine.Where(t => t.IdEngine == id).FirstOrDefaultAsync());
         }
     }
 }
